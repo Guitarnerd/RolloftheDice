76b21027c0ae4d9d9208ef155c803b4f20861373
DanielJutila <45379504+DanielJutila@users.noreply.github.com>
2022-07-16 15:07:28 -0500

    Added DiceFunctions
    Added methods for the dice and timer for when they activate

--- Project Dice/Assets/DieSideDetection.cs
+++ Project Dice/Assets/DieSideDetection.cs
@@ -3,29 +3,54 @@ using UnityEngine;
 public class DieSideDetection : MonoBehaviour
 {
 
+    //Cooldown in seconds for when next ability will be chosen
+    public float cooldown = 60;
+
+
+    //No need to change the currentTimer
+    private float currentTimer = 0;
+
+
+    public Rigidbody player;
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
-
-        if (Input.GetKeyDown(KeyCode.E))
+       
+        if (Time.time > currentTimer)
         {
-            Debug.Log("Key registered");
-
-            readHighPoint();
+            currentTimer = Time.time + cooldown;
+            action();
         }
+
     }
+
+
+    //What happens once the timer is completed
+    void action()
+    {
+        //the animations will be above readHighPoint
+
+
+        readHighPoint();
+
+
+        //the setting of Dice states will be below readHighPoint
+    }
+
+
     //reads the highest point of the Die
     void readHighPoint()
     {
         GameObject[] numbers = GameObject.FindGameObjectsWithTag("Numbers");
 
-        //The number on the die
+        //saves the current die number that is the same as highest position
         int number = 0;
-        //the highest position
+
+        //saves the highest position
         float height = 0.0f;
 
-        Debug.Log("Read High Points");
 
 
         foreach(GameObject name in numbers)
@@ -36,15 +61,53 @@ public class DieSideDetection : MonoBehaviour
                 number = int.Parse(name.name);
             }
         }
-        announceSide(number);
+        roll(number);
     }
 
-    //Action depending on what the number is
-    void action() { 
-    }
-
-    void announceSide(int Number)
+    //Checks the number and sends to the different states
+    public void roll(int number)
     {
-        Debug.Log("" + Number);
+        if (number == 1)
+            criticalFail();
+
+        if (number == 2 || number == 3 || number == 4 || number == 5 || number == 6 || number == 7)
+            ice();
+
+        if (number == 8 || number == 9 || number == 10 || number == 11 || number == 12 || number == 13)
+            fire();
+
+
+        if (number == 14 || number == 15 || number == 16 || number == 17 || number == 18 || number == 19)
+            electricity();
+
+        if (number == 20)
+            criticalSuccess();
+
+    }
+    void criticalFail()
+    {
+        Debug.Log("FAIL");
+    }
+    void ice()
+    {
+        Debug.Log("Ice");
+
+    }
+    void fire()
+    {
+        Debug.Log("Fire");
+
+    }
+    void electricity()
+    {
+        Debug.Log("elec");
+
+    }
+    void criticalSuccess()
+    {
+        Debug.Log("Win");
+
     }
 }
+
+
