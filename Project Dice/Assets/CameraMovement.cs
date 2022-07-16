3261057552159efd9297463ed532459dd2fea804
DanielJutila <45379504+DanielJutila@users.noreply.github.com>
2022-07-15 23:11:00 -0500

    Camera moves up and down now

--- Project Dice/Assets/CameraMovement.cs
+++ Project Dice/Assets/CameraMovement.cs
@@ -6,19 +6,20 @@ public class CameraMovement : MonoBehaviour
 {
 
     // Start is called before the first frame update
-    public float turnSpeed = 4.0f;
+    public float turnSpeed = 5.5f;
     public Transform player;
 
     private Vector3 offset;
 
     void Start()
     {
-        offset = new Vector3(player.position.x + 6.0f, player.position.y + 2.0f, player.position.z + 6.0f);
+        offset = new Vector3(player.position.x + 4.0f, player.position.y + 1.0f, player.position.z + 4.0f);
     }
 
     void LateUpdate()
     {
         offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.right) * offset;
         transform.position = player.position + offset;
         transform.LookAt(player.position);
     }
