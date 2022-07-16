de612e71061dc1502cdc8fe8aa385e7929ef8388
DanielJutila <45379504+DanielJutila@users.noreply.github.com>
2022-07-16 17:21:21 -0500

    made some functionality

--- Project Dice/Assets/DieSideDetection.cs
+++ Project Dice/Assets/DieSideDetection.cs
@@ -4,7 +4,7 @@ public class DieSideDetection : MonoBehaviour
 {
 
     //Cooldown in seconds for when next ability will be chosen
-    public float cooldown = 60;
+    public float cooldown = 60f;
 
 
     //No need to change the currentTimer
@@ -15,11 +15,10 @@ public class DieSideDetection : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate()
-    {
-        
-       
+    {       
         if (Time.time > currentTimer)
         {
+            resetAffects();
             currentTimer = Time.time + cooldown;
             action();
         }
@@ -43,6 +42,8 @@ public class DieSideDetection : MonoBehaviour
     //reads the highest point of the Die
     void readHighPoint()
     {
+        
+
         GameObject[] numbers = GameObject.FindGameObjectsWithTag("Numbers");
 
         //saves the current die number that is the same as highest position
@@ -64,9 +65,14 @@ public class DieSideDetection : MonoBehaviour
         roll(number);
     }
 
+
+
+
     //Checks the number and sends to the different states
     public void roll(int number)
     {
+
+        
         if (number == 1)
             criticalFail();
 
@@ -84,30 +90,148 @@ public class DieSideDetection : MonoBehaviour
             criticalSuccess();
 
     }
+
+
+    private bool iceBool;
+    private bool fireBool;
+    private bool elec;
+    private bool win;
+    void resetAffects()
+    {
+
+        if (iceBool)
+        {
+            //RESET ice
+            ice();
+            return;
+        }
+        if (fireBool)
+        {
+            //RESET fire
+            fire();
+            return;
+        }
+        if (elec)
+        {
+            //RESET elec
+            electricity();
+            return;
+        }
+        if (win)
+        {
+            //RESET win
+            criticalSuccess();
+            return;
+        }
+            
+    }
+
+
+    //All Crit fail stuff
     void criticalFail()
     {
-        Debug.Log("FAIL");
+         
+            player.velocity = randomLaunch(2f, maxFail);
+         
     }
+
+    public float maxFail;
+    private Vector3 randomLaunch(float min, float max)
+    {
+        var x = Random.Range(min, max);
+        var y = Random.Range(min, max);
+        var z = Random.Range(min, max);
+        return new Vector3(x, y, z);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public float playerMassWithIce = .85f;
     void ice()
     {
-        Debug.Log("Ice");
+        if (!iceBool)
+        {
+            iceBool = true;
+            Debug.Log("Ice");
+
+            player.mass = playerMassWithIce;
+        } else
+        {
+            player.mass = 1f;
+            iceBool = false;
+        }
 
     }
     void fire()
     {
-        Debug.Log("Fire");
+        if (!fireBool)
+        {
+            fireBool = true;
+            Debug.Log("Fire");
+        } else
+        {
+            fireBool = false;
+        }
 
     }
     void electricity()
     {
-        Debug.Log("elec");
-
+        if (!elec)
+        {
+            elec = true;
+            Debug.Log("elec");
+        } else
+        {
+            elec = false;
+        }
     }
+
+
+
+
+
+
+
+    public float floatHeight;
+    public float floatTime;
+    private Vector3 startPosition;
+    public Vector3 tempPosistion;
+    private bool floating;
+    private float startFloat = Time.time;
+
     void criticalSuccess()
     {
-        Debug.Log("Win");
 
+        tempPosistion = new Vector3(0, floatHeight, 0);
+        startPosition = player.position;
+
+        startPosition = startPosition + tempPosistion;
+
+        if (!floating)
+        {
+            
+            player.transform.position = startPosition;
+            floating = true;
+
+            if((Time.time - startFloat)> 5)
+            {
+
+            }
+        }
+        Debug.Log("Win");
+        
     }
+
+   
+   
 }
 
 
--- Project Dice/Assets/PlayerController.cs
+++ Project Dice/Assets/PlayerController.cs
@@ -32,12 +32,18 @@ public class PlayerController : MonoBehaviour
         }
 
         Vector3 direction = new Vector3(xInput, 0f, zInput);
-        if (direction.magnitude >= 0.1f)
+        if (direction.magnitude >= 0.1f && isGrounded)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
             rb.AddForce(moveDir * moveSpeed);
+        } else if(direction.magnitude>= 0.1f && !isGrounded)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            rb.AddForce(moveDir * moveSpeed/2);
         }
 
     }
