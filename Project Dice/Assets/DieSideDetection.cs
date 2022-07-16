425158704ab983dde100c967a9fcd2363818e025
DanielJutila <45379504+DanielJutila@users.noreply.github.com>
2022-07-16 02:17:35 -0500

    Got the dice sides working

--- Project Dice/Assets/DieSideDetection.cs
+++ Project Dice/Assets/DieSideDetection.cs
@@ -1,18 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DieSideDetection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Key registered");
+
+            readHighPoint();
+        }
+    }
+    //reads the highest point of the Die
+    void readHighPoint()
+    {
+        GameObject[] numbers = GameObject.FindGameObjectsWithTag("Numbers");
+
+        //The number on the die
+        int number = 0;
+        //the highest position
+        float height = 0.0f;
+
+        Debug.Log("Read High Points");
+
+
+        foreach(GameObject name in numbers)
+        { 
+            if(name.transform.position.y > height)
+            {
+                height = name.transform.position.y;
+                number = int.Parse(name.name);
+            }
+        }
+        announceSide(number);
+    }
+
+    //Action depending on what the number is
+    void action() { 
+    }
+
+    void announceSide(int Number)
+    {
+        Debug.Log("" + Number);
     }
 }
